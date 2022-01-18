bc69bd9191ef61b328ac6a05c6be3e277be445ab
Shindusik <shindusik@manokorea.com>
2022-01-18 14:07:01 +0800

    initial commit

---
+++ src/ch01/Hello.java
@@ -0,0 +1,15 @@
+package ch01;
+
+/*
+작성자: 신두식
+작성일: 2022.01.17
+*/
+
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, welcome to the java world!");
+		System.out.println("안녕하세요? 저는 신두식입니다.");
+	} // end of main
+} // end of class
