7909eaef1d6fd2b58e0f70cffddbcfef7497d267
Shindusik <shindusik@manokorea.com>
2022-01-24 13:42:31 +0800

    chapter03 나머지 소스 업로드

---
+++ src/ch03/Exercise02.java
@@ -0,0 +1,12 @@
+package ch03;
+
+public class Exercise02 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		int z = (++x) + (y--); // 11 + 20 = 31
+		System.out.println(z);
+	}
+
+}
---
+++ src/ch03/Exercise03.java
@@ -0,0 +1,11 @@
+package ch03;
+
+public class Exercise03 {
+
+	public static void main(String[] args) {
+		int score = 85;
+		String result = (!(score > 90) ? "가" : "나"); // "가"
+		System.out.println(result);
+	}
+
+}
