d79130eba8aa51afd9d99ff1790cc9104038ee84
Shindusik <shindusik@manokorea.com>
2022-02-11 13:45:58 +0800

    chapter 06 Class 소스 업로드

--- src/ch03/CharOperationExample.java
+++ src/ch03/CharOperationExample.java
@@ -1,14 +1,21 @@
 package ch03;
 
+import java.lang.Character.UnicodeBlock;
+
 public class CharOperationExample {
 
 	public static void main(String[] args) {
-		char c1 = 'A' + 1;
+		char c1 = 'A' + 'B';
 		char c2 = 'A';
-		char c3 = (char) (c2 + 1); // 컴파일 에러
+		char c3 = (char) (c2 + 1);
 		System.out.println("c1: " + c1);
 		System.out.println("c2: " + c2);
 		System.out.println("c3: " + c3);
+		
+		int c4 = 'A' + 'B';
+		System.out.println(c4);
+		System.out.println(Integer.toBinaryString(c1));
+		System.out.println(UnicodeBlock.of(c1));
 	}
 
 }
