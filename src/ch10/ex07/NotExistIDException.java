3b327d2b11e7a646bb05ec17e1d27ebe55ca7427
Shindusik <shindusik@manokorea.com>
2022-02-23 17:31:07 +0800

    chapter 10 예외 처리 소스 업로드

---
+++ src/ch10/ex07/NotExistIDException.java
@@ -0,0 +1,11 @@
+package ch10.ex07;
+
+public class NotExistIDException extends Exception {
+	public NotExistIDException() {
+		
+	}
+	
+	public NotExistIDException(String message) {
+		super(message);
+	}
+}
