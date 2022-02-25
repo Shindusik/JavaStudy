60c4890709f86367f0f88809f61d18765125a655
Shindusik <shindusik@manokorea.com>
2022-02-25 17:08:36 +0800

    Chapter11 기본 ATP 소스 일부 업로드

---
+++ src/ch11/HashCodeExample.java
@@ -0,0 +1,30 @@
+package ch11;
+
+import java.util.Objects;
+
+public class HashCodeExample {
+
+	// 정적 멤버 클래스
+	public static class Student {
+		int sno;
+		String name;
+
+		Student(int sno, String name) {
+			this.sno = sno;
+			this.name = name;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(sno, name);
+		}
+	}
+
+	public static void main(String[] args) {
+		Student s1 = new Student(1, "홍길동");
+		Student s2 = new Student(1, "홍길동");
+		System.out.println(s1.hashCode());
+		System.out.println(Objects.hashCode(s2));
+	}
+
+}
