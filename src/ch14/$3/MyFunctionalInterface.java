d82bd392e3d53370f59c92acaa15c09ebb3e26d2
Shindusik <shindusik@manokorea.com>
2022-03-21 12:40:08 +0800

    Chapter 14 소스 삭제

--- src/ch14/$3/MyFunctionalInterface.java
+++
@@ -1,6 +0,0 @@
-package ch14.$3;
-
-@FunctionalInterface
-public interface MyFunctionalInterface {
-	public void method(int x);
-}
--- src/ch14/MyFunctionalInterface.java
+++
@@ -1,6 +0,0 @@
-package ch14;
-
-@FunctionalInterface
-public interface MyFunctionalInterface {
-	public void method();
-}
